004deb08655e373a311ba4ed04f6812b22040c5b
yossarianirving <acward96@gmail.com>
2019-11-30 12:21:12 -0500

    Added CORS header

--- src/main/java/GradeApi/RequirementsController.java
+++ src/main/java/GradeApi/RequirementsController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @SpringBootApplication
 @Component
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/concentrationRequirements")
 public class RequirementsController {
